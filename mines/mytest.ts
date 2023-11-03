4d70fa1315a272fdddfe398ec10ffd21364d0395
Cristina <cristinamateospaez@gmail.com>
2023-11-03 15:11:11 +0100

    demo

--- mines/mytest.ts
+++ mines/mytest.ts
@@ -6,4 +6,17 @@ function allSprites<T>(sprites: T[]) :T[] {
     return new Array<T>().concat(sprites);
 }
 
-let redTeam = allSprites([]);
+let redTeam = allSprites(['Red Ribbon', 'Wooly', 'Laine', 'Essa', 'Betty', 'Chamy', 'Ole', 'Sue', 'Magic', 'Bali', 'Mick', 'Woohoo']);
+let orangeTeam = allSprites(['Oran', 'Bran', 'Decoy', 'Woody', 'Pierre', 'Tilus', 'Stony', 'Rosh', 'Alpen', 'Rocky', 'Valie', 'Fen']);
+
+// console.log(redTeam);
+// console.log(redTeam[0]);
+// console.log(redTeam.length);
+// console.log(orangeTeam + '' +  redTeam);
+
+function identity<T, U>(value:T, message: U) :T {
+    return value;
+}
+
+let s = identity('team1', 'team2');
+console.log(s);
