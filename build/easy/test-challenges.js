f3cd6f2561f6690cde993a9b1a7c49831214175c
Cristina <cristinamateospaez@gmail.com>
2023-11-03 10:13:53 +0100

    updating challenges

--- build/easy/test-challenges.js
+++ build/easy/test-challenges.js
@@ -1,9 +1,26 @@
 "use strict";
 /**
- * WARM-UP and EASY TS Challenges
+ * WARM-UP and EASY TS Challenges explained
  */
 // 01 -> Hello World
 console.log("**** Exercise 1 ****");
 const result = 'Hi everyone, hello world';
 console.log(result);
-// 02 ->
+// 04 -> Pick
+console.log("**** Exercise 4 ****");
+let result04 = {
+    title: 'Typescript tutorial',
+    //description: 'finish the easy ts-challenge tutorial',
+    completed: false,
+    author: 'Cristina'
+};
+console.log(result04);
+console.log(result04.author);
+// function tooManyScoops(dessert: IceCream) {
+//     if (dessert.scoops >= 4) {
+//        return dessert.scoops + ' is too many scoops!';
+//     } else {
+//        return 'Your order will be ready soon!';
+//     }
+//  }
+//  console.log(tooManyScoops({flavor: 'vanilla', scoops: 5}));
--- easy/test-challenges.ts
+++ easy/test-challenges.ts
@@ -1,5 +1,5 @@
 /**
- * WARM-UP and EASY TS Challenges
+ * WARM-UP and EASY TS Challenges explained
  */
 
 // 01 -> Hello World
@@ -8,4 +8,30 @@ type HelloWorld = string // expected to be a string
 const result: HelloWorld = 'Hi everyone, hello world';
 console.log(result);
 
-// 02 ->
+// 04 -> Pick
+console.log("**** Exercise 4 ****");
+interface Todo {
+    title: string;
+    description?: string; //opcional
+    completed: boolean;
+    author: string;
+}
+
+let result04: Todo = {
+    title: 'Typescript tutorial',
+    //description: 'finish the easy ts-challenge tutorial',
+    completed: false,
+    author: 'Cristina'
+}
+console.log(result04);
+console.log(result04.author);
+
+// function tooManyScoops(dessert: IceCream) {
+//     if (dessert.scoops >= 4) {
+//        return dessert.scoops + ' is too many scoops!';
+//     } else {
+//        return 'Your order will be ready soon!';
+//     }
+//  }
+ 
+//  console.log(tooManyScoops({flavor: 'vanilla', scoops: 5}));
