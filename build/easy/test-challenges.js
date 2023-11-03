3b4ed49d932c3694f7ba7ba148ac50c9834758e4
Cristina <cristinamateospaez@gmail.com>
2023-11-03 09:05:39 +0100

    exercise done

---
+++ build/easy/test-challenges.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * WARM-UP and EASY TS Challenges
+ */
+// 01 -> Hello World
+console.log("**** Exercise 1 ****");
+const result = 'Hi everyone, hello world';
+console.log(result);
+// 02 ->
---
+++ easy/test-challenges.ts
@@ -0,0 +1,11 @@
+/**
+ * WARM-UP and EASY TS Challenges
+ */
+
+// 01 -> Hello World
+console.log("**** Exercise 1 ****");
+type HelloWorld = string // expected to be a string
+const result: HelloWorld = 'Hi everyone, hello world';
+console.log(result);
+
+// 02 ->
